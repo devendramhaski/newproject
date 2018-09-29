5de1090f785a7dc954e3f268ad3079cf5b51e9a5
devendramhaski <devendramhaski25@gmail.com>
2018-09-29 13:29:36 +0530

    hello umesh

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+include("header.php");
+?>
+<div id="content">
+
+			<div id="login-box"><h4>Login</h4>
+				<form>
+					<table align="center">
+									<tr>
+										<td>Username</td>
+										<td> <input type="text" placeholder="Username" class ="input"></td>
+									</tr>
+									<tr>
+										<td>Password</td>
+										<td><input type="password" placeholder="password" class="input"></td>
+									</tr>
+									<tr >
+										
+										<td colspan="2" align="right"><input type="submit" value="Login" name="login" class="btn"></td>
+									</tr>
+								</table>
+						</form>
+					</div>
+					</div>
+
+<?php
+include("footer.php");
+?>
